d0c0660c7d4033bc9deb27b4ecf370a76c5ed9da
Libaan Ibrahim <liibaanzaki5@gmail.com>
2026-04-21 13:37:54 +0000

    Try fixing this error: `Runtime ReferenceError: Link is not defined. Err

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,7 @@
-
 "use client";
 
 import { useState } from "react";
+import Link from "next/link";
 import Header from "@/components/layout/Header";
 import BottomNav from "@/components/layout/BottomNav";
 import { useApp } from "@/lib/context";
