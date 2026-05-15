fb426da3cff3fb15055ec7b01f5aa6fd750adf2e
Libaan Ibrahim <liibaanzaki5@gmail.com>
2026-05-15 17:45:10 +0000

    Try fixing this error: `Console Error: Invalid src prop (https://i.ibb.c

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'files.catbox.moe',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
         port: '',
         pathname: '/**',
       },
