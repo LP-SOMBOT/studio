dce2bf41a67cc12c26ee7d02ef9a4414500b03b7
Libaan Ibrahim <liibaanzaki5@gmail.com>
2026-05-18 11:27:31 +0000

    Update this card: title: Ranking, Description: iib sameey Si aad u gasho

--- src/components/views/HomeView.tsx
+++ src/components/views/HomeView.tsx
@@ -159,8 +159,8 @@ export default function HomeView() {
                   <Trophy className="w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14" />
                </div>
                <div className="space-y-0.5 md:space-y-1">
-                  <h3 className="text-xl md:text-3xl lg:text-5xl font-headline font-bold tracking-tight uppercase">Global Ranking</h3>
-                  <p className="text-white/80 text-[10px] md:text-base lg:text-xl font-medium">Earn your place in the Hall of Fame!</p>
+                  <h3 className="text-xl md:text-3xl lg:text-5xl font-headline font-bold tracking-tight uppercase">Ranking</h3>
+                  <p className="text-white/80 text-[10px] md:text-base lg:text-xl font-medium">iib sameey Si aad u gasho kaalmaha hore una heshid discount gaaraya ilaa %3, halkii iibin top up waxaad Ku heleesaa 1 points (pts). Hadiyado kalena coming soon I.a.</p>
                </div>
             </div>
             <div className="mt-6 md:mt-0 w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 bg-white/20 rounded-full flex items-center justify-center group-hover:bg-white/30 transition-colors">
