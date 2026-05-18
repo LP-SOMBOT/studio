d07129cf5d5147df4d0ac773d99844556bd3f9f5
Libaan Ibrahim <liibaanzaki5@gmail.com>
2026-05-18 14:14:20 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { useState, useMemo, useEffect } from "react";
@@ -114,7 +113,7 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/accordion";
+} from "@/components/ui/accordion";
 import {
   Sheet,
   SheetContent,
